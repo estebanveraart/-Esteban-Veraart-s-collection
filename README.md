061acd36a7d4eef73588761f8f5232b1aee9a25a
estebanveraart <esteban.veraart@gmail.com>
2021-09-16 16:17:57 +0200

    commit final version js

---
+++ README.md
@@ -0,0 +1,26 @@
+# The Collection
+
+## First Step
+
+At first, I created this page only using html and css to have a global vision.
+
+## Second Step
+Then, I went over my JS file, where I created the tags, classes and text. The method is to follow my html file and create the elements one by one. When the JS file was finished, I deleted everything in the html file to leave only the "header", "main" and "footer"
+
+<img src="images/html.png">
+
+The page is responsive, which makes it easy to visit the page on any screen.
+
+on a phone :
+
+<img src="images/responsive gsm.png">
+
+on a normal screen :
+
+<img src="images/site.jpeg">
+
+on a big screen :
+
+<img src="images/responsive max.jpeg">
+
+The goal of this exercise was to put into practice all the information assimilated for 3 weeks.
--- collection.js
+++ collection.js
@@ -1,90 +1,167 @@
+let header = document.querySelector('header');
+let newDiv = document.createElement('div');
+let newDivAttribute = document.createAttribute('header');
+let newA = document.createElement('a');
+let newAAttribute= document.createAttribute('active');
+
+newA.setAttributeNode(newAAttribute);
+newDiv.setAttributeNode(newDivAttribute);
+newDiv.appendChild(newA);
+header.appendChild(newDiv);
+
+let newH1 = document.createElement('h1');
+let newTextH1 = document.createTextNode("My manhua collection");
+newH1.appendChild(newTextH1);
+header.appendChild(newH1);
+
 const BdAsiatique = [
     {
-      name: 'Gu Daoist Master',
-      autor: 'Gu Zhen Ren',
-      releaseYear: '2018',
-      Chapitre : '96 chapitres',
-      genre: ['Reincarnation, Fantastic, Martial Arts, Comedy'],
+        image : " images/GuDaoist.jpeg ",
+        alt : "Personnage Principale : Fang Yuan",
+        class : "Gu",
+        name: 'Gu Daoist Master',
+        autor: ' écrit par "Gu Zhen Ren"',
+        releaseYear: ' en 2018.',
+        Chapitre : ' Il y a actuellement 96 chapitres',
+        genre: [` et c'est un manhua de type : Reincarnation, Fantastic, Martial Arts, Comedy`],
     },
     {
+        image : " images/MartialPeak.jpeg ",
+        alt : "Personnage Principale : Yang Kai",
+        class : "Peak",
         name: 'Martial Peak',
-        autor: 'Pikapi Momo',
-        releaseYear: '2018',
-        Chapitre : '1499 chapitres',
-        genre: ['Fantastic, Comedy, Martial Arts'],
+        autor: ' écrit par "Pikapi Momo"',
+        releaseYear: ' en 2018.',
+        Chapitre : ' Il y a actuellement 1499 chapitres',
+        genre: [` et c'est un manhua de type : Fantastic, Comedy, Martial Arts`],
     },
     {
+        image : " images/GreatImmortal.jpeg ",
+        alt : "Personnage Principale : Li Taixuan",
+        class : "Great",
         name: 'Im the great immortal',
-        autor: 'June snow studio',
-        releaseYear: '2019',
-        Chapitre : '179 chapitres',
-        genre: ['Reincarnation, Martial Arts, Fantastic, Comedy'],
+        autor: ' écrit par "June snow studio"',
+        releaseYear: ' en 2019.',
+        Chapitre : ' Il y a actuellement 179 chapitres',
+        genre: [`  et c'est un manhua de type : Reincarnation, Martial Arts, Fantastic, Comedy`],
     },
     {
+        image : " images/RebirthCultivator.jpeg ",
+        alt : "Personnage Principale : Chen Fan",
+        class : "Rebirth",
         name: 'Rebirth of the urban immortal cultivator',
-        autor: 'Shiu Jianshen',
-        releaseYear: '2018',
-        Chapitre : '431 chapitres',
-        genre: ['Reincarnation, Martial Arts, Fantastic'],
+        autor: ' écrit par "Shiu Jianshen"',
+        releaseYear: ' en 2018.',
+        Chapitre : ' Il y a actuellement 431 chapitres',
+        genre: [` et c'est un manhua de type : Reincarnation, Martial Arts, Fantastic`],
     },
     {
+        image : " images/TalesDemons.jpeg ",
+        alt : "Personnage Principale : Nie Li",
+        class : "Tales",
         name: 'Tales of demons and gods',
-        autor: 'Snail',
-        releaseYear: '2015',
-        Chapitre : '345 chapitres',
-        genre: ['Martial Arts, Fantastic, Comedy'],
+        autor: ' écrit par "Snail"',
+        releaseYear: ' en 2015.',
+        Chapitre : ' Il y a actuellement 345 chapitres',
+        genre: [` et c'est un manhua de type : Martial Arts, Fantastic, Comedy`],
     },
     {
+        image : " images/MartialReings.jpeg ",
+        alt : "Personnage Principale : Ming Ye",
+        class : "Reings",
         name: 'Martial art reigns',
-        autor: 'Pojie',
-        releaseYear: '2019',
-        Chapitre : '317 chapitres',
-        genre: ['Martial Art, Fantastic, Comedy'],
+        autor: ' écrit par "Pojie"',
+        releaseYear: ' en 2019.',
+        Chapitre : ' Il y a actuellement 317 chapitres',
+        genre: [` et c'est un manhua de type : Martial Art, Fantastic, Comedy`],
     },
     {
+        image : " images/GodKing.jpeg ",
+        alt : "Personnage Principale : Wu Fu",
+        class : "God",
         name: 'The strongest god king',
-        autor: 'Edje man cartoon',
-        releaseYear: '2019',
-        Chapitre : '115 chapitres',
-        genre: ['Reincarnation, Fantastic, Martial Arts, Comedy'],
+        autor: ' écrit par "Edje man cartoon"',
+        releaseYear: ' en 2019.',
+        Chapitre : ' Il y a actuellement 115 chapitres',
+        genre: [` et c'est un manhua de type : Reincarnation, Fantastic, Martial Arts, Comedy`],
+
       },
       {
+        image : " images/DragonBody.jpeg ",
+        alt : "Personnage Principale : Mo Fan",
+        class : "Dragon",
         name: 'I have a dragon in my body',
-        autor: 'island project',
-        releaseYear: '2017',
-        Chapitre : '287 chapitres',
-        genre: ['Reincarnation, Marial Arts, Fantastic, Comedy'],
+        autor: ' écrit par "island project"',
+        releaseYear: ' en 2017.',
+        Chapitre : ' Il y a actuellement 287 chapitres',
+        genre: [` et c'est un manhua de type : Reincarnation, Marial Arts, Fantastic, Comedy`],
       },
       {
+        image : " images/BaiLian.jpeg ",
+        alt : "Personnage Principale : Luo Zeng",
+        class : "Bai",
         name: 'Bai Lian Cheng Shen',
-        autor: 'Ranzai studio',
-        releaseYear: '2015',
-        Chapitre : '813 chapitres',
-        genre: ['Martial Arts, Fantastic, Comedy'],
+        autor: ' écrit par "Ranzai studio"',
+        releaseYear: ' en 2015.',
+        Chapitre : ' Il y a actuellement 813 chapitres',
+        genre: [` et c'est un manhua de type : Martial Arts, Fantastic, Comedy`],
       },
       {
-        name: 'Yuan Zun',
-        autor: 'Tian Can Tu dou',
-        releaseYear: '2018',
-        Chapitre : '343 chapitres',
-        genre: ['Martial Arts, Fantastic, Comedy'],
+        image : " images/YuanZun.jpeg ",
+        alt : "Personnage Principale : Yuan Zun",
+        class : "Yuan",
+        name: 'Yuan Zun,',
+        autor: ' écrit par "Tian Can Tu dou"',
+        releaseYear: ' en 2018.',
+        Chapitre : ' Il y a actuellement 343 chapitres,',
+        genre: [` et c'est un manhua de type : Martial Arts, Fantastic, Comedy`],
       },
     ]
 
+
 let n = 0;
-let Sections = document.querySelectorAll('section');
-for(n=0;n < BdAsiatique.length;n++){
-    console.log(BdAsiatique[n]);
-    console.log(Sections[n]);
-    let newName= document.createTextNode(BdAsiatique[n].name);
-    let newAutor= document.createTextNode(BdAsiatique[n].autor);
-    let newReleaseDate= document.createTextNode(BdAsiatique[n].releaseYear);
-    let newChapter= document.createTextNode(BdAsiatique[n].Chapitre);
-    let newGender= document.createTextNode(BdAsiatique[n].genre);
-    Sections[n].appendChild(newName);
-    Sections[n].appendChild(newAutor);
-    Sections[n].appendChild(newReleaseDate);
-    Sections[n].appendChild(newChapter);
-    Sections[n].appendChild(newGender);
+
+while(n<BdAsiatique.length){
+
+let main = document.querySelector('main');
+let newSection = document.createElement('section');
+let image = document.createElement('img');
+let newStrong = document.createElement('strong');
+let newH2 = document.createElement('h2');
+let newTextH2 = document.createTextNode(BdAsiatique[n].name);
+let newH3 = document.createElement('h3');
+let newTextH3 = document.createTextNode(BdAsiatique[n].autor);
+let newH4 = document.createElement('h4');
+let newTextH4 = document.createTextNode(BdAsiatique[n].releaseYear);
+let newP = document.createElement('p');
+let newTextP = document.createTextNode(BdAsiatique[n].Chapitre+BdAsiatique[n].genre);
+let newDivMain = document.createElement('div');
+let imagesDiv = document.createAttribute('src');
+imagesDiv.value = BdAsiatique[n].image
+let imagesDivAlt = document.createAttribute('alt')
+imagesDivAlt.value = BdAsiatique[n].alt
+
+image.setAttributeNode(imagesDivAlt);
+image.setAttributeNode(imagesDiv);
+newDivMain.appendChild(image);
+newSection.appendChild(newDivMain);
+
+newStrong.appendChild(newTextH2);
+newH2.appendChild(newStrong);
+newSection.appendChild(newH2);
+
+newH3.appendChild(newTextH3);
+newSection.appendChild(newH3);
+
+newH4.appendChild(newTextH4);
+newSection.appendChild(newH4);
+
+newP.appendChild(newTextP);
+newSection.appendChild(newP);
+
+newSection.className = BdAsiatique[n].class;
+main.appendChild(newSection);
+
+n++;
 }
 
